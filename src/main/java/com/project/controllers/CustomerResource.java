df7af78c186413edc89f068d9266c0085752c52d
pallavi35 <pallavi.gulati35@gmai.com>
2018-06-23 14:58:30 +0530

    Testing Commit

--- src/main/java/com/project/controllers/CustomerResource.java
+++ src/main/java/com/project/controllers/CustomerResource.java
@@ -4,7 +4,6 @@ import org.springframework.http.MediaType;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-
 @RestController
 @RequestMapping("/customers")
 public class CustomerResource {
